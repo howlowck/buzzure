749dd7649ee581e700ff38479ff7ea558ef0d39a
Hao Luo <howlowck@gmail.com>
2021-12-26 15:37:18 -0600

    removed hardcode games

--- src/services/storage/memoryStorage.ts
+++ src/services/storage/memoryStorage.ts
@@ -1,30 +1,6 @@
 import { StorageClient } from '../../types'
 
-const data: Record<string, { [key: string]: any }> = {
-  test: {
-    __game__: {
-      gameName: 'Main Game',
-      adminPassword: 'test',
-      teams: [
-        {
-          teamId: 'aR7TeGvMo-CGLbjIjfefd',
-          color: '#0bb873',
-          name: 'Team 1',
-        },
-        {
-          teamId: 'El7tjOh4YAWBJvvUOzAYs',
-          color: '#1900ff',
-          name: 'Team 2',
-        },
-        {
-          teamId: 'EUGutTbx_jcdN5dh57boI',
-          color: '#a80bbd',
-          name: 'Team 3',
-        },
-      ],
-    },
-  },
-}
+const data: Record<string, { [key: string]: any }> = {}
 
 const memoryStorage: StorageClient = {
   getItem: (partition, name, defaultValue) => {
